87295fe03a735e0c3ba9efce000e9b298beb78e1
Lily Isnaini Astriningsih <37197492+lilyastri@users.noreply.github.com>
2018-07-30 06:17:45 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$hostname_conn = "localhost";
-$database_conn = "shopping";
-$username_conn = "root";
-$password_conn = "";
-$conn = mysql_connect($hostname_conn, $username_conn, $password_conn) or die(mysql_error()); 
-?>
